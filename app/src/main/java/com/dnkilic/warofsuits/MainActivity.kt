033fbdcaab36bc7f8fed855ad26f7140ee8a9794
Dogan Kilic <dnkilic@outlook.com>
2022-03-14 01:15:21 +0100

    Remove unused preview

--- app/src/main/java/com/dnkilic/warofsuits/MainActivity.kt
+++ app/src/main/java/com/dnkilic/warofsuits/MainActivity.kt
@@ -4,16 +4,10 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
-import com.dnkilic.uicomponents.components.PlayCard
-import com.dnkilic.warofsuits.navigation.WarOfSuitsNavigation
 import com.dnkilic.uicomponents.theme.WarOfSuitsTheme
 import com.dnkilic.warofsuits.game.ui.GameViewModel
+import com.dnkilic.warofsuits.navigation.WarOfSuitsNavigation
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -31,16 +25,6 @@ class MainActivity : ComponentActivity() {
                     gameViewModel = gameViewModel
                 )
             }
-        }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    WarOfSuitsTheme {
-        Column(Modifier.fillMaxSize()) {
-            PlayCard()
         }
     }
 }
